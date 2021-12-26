2261484bf2adb15ccb3dbd3d8870fd82b92dc3df
Eric Harper <harper28@pdx.edu>
2021-12-25 21:16:33 -0800

    First commit, making home page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Home } from "./Pages/Home/Home";
+import "./App.css";
 
 function App() {
+  const location = useLocation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes location={location} key={location.pathname}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="*" render={() => <Navigate to="/" />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./home.css";
+
+export const Home = () => {
+  return (
+    <div>
+      <h2>GOOD</h2>
+      <p>Very good</p>
+    </div>
+  );
+};
